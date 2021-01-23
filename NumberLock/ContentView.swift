ce7fcc072731e2fafdfdfdea617008c697c92152
张亚飞 <672233580@qq.com>
2021-01-23 14:40:01 +0800

    Initial Commit

---
+++ NumberLock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NumberLock
+//
+//  Created by 张亚飞 on 2021/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NumberLock/NumberLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberLockApp.swift
+//  NumberLock
+//
+//  Created by 张亚飞 on 2021/1/23.
+//
+
+import SwiftUI
+
+@main
+struct NumberLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
